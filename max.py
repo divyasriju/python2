3995969ce86b91622fcc86cedb7def6c40266fd2
srijuvr <srijuvr@gmail.com>
2023-03-04 12:02:23 -0500

    py

---
+++ max.py
@@ -0,0 +1,9 @@
+#To read the input value from the user 
+num1 = int(input('Enter the first number: '))
+num2 =int( input('Enter the second number: '))
+
+#Finding the maximum value using Python max() funtion
+maximum = max(num1, num2)
+
+#Displaying the maximum number
+print("The maximum number from {0} and {1} is {2} ".format(num1,num2,maximum))
---
+++ new1.py
@@ -0,0 +1,18 @@
+print('Hello, Python!')
+print("Hello, Python!")
+print('''Hello, Python!''')
+print('"Hello, Python"')
+print("'Hello, Python'")
+
+#Python Add Two Numbers Program
+number1 = 5
+number2 = 4
+# Add two numbers
+sum = number1 + number2
+# Display the sum of the two numbers
+print('The sum of the two numbers is:', sum)
+
+num1 = int(input('Enter first number: ')) 
+num2 = int(input('Enter second number: ')) 
+sum = num1 + num2  
+print('The sum of the numbers is', sum)
